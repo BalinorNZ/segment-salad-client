73759711d1695c7d88923732656faa80c91fb9d0
Nic Bathgate <balinornz@gmail.com>
2017-12-04 00:54:56 +1300

    change vars to consts

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -251,9 +251,9 @@ function formatDistance(d) {
 }
 function secondsToHms(d) {
   d = Number(d);
-  var h = Math.floor(d / 3600);
-  var m = Math.floor(d % 3600 / 60);
-  var s = Math.floor(d % 3600 % 60);
+  const h = Math.floor(d / 3600);
+  const m = Math.floor(d % 3600 / 60);
+  const s = Math.floor(d % 3600 % 60);
   return h > 0 ? ('0' + h).slice(-2) + ":" + ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2)
     : ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2);
 }
